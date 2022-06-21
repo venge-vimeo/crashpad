9ad3b4dbb13482f9ce05df561b54e527a3b998ff
Vladimir Engelgardt <volodymyr.engelgardt@vimeo.com>
2022-06-21 11:49:47 +0300

    ForkAndSpawn

--- handler/crash_report_report_tool.cc
+++ handler/crash_report_report_tool.cc
@@ -4,7 +4,7 @@
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_APPLE)
-#include "util/posix/double_fork_and_exec.h"
+#include "util/posix/fork_and_spawn.h"
 #elif BUILDFLAG(IS_WIN)
 #include <sstream>
 
@@ -31,8 +31,8 @@ void RunTool(bool simulation,
   argv.push_back("--dump");
   argv.push_back(dump_path.value());
 
-  if (!DoubleForkAndExec(argv, nullptr, -1, false, nullptr)) {
-    LOG(ERROR) << "DoubleForkAndExec";
+  if (!ForkAndSpawn(argv, nullptr, -1, false, nullptr)) {
+    LOG(ERROR) << "ForkAndSpawn";
   }
 #elif BUILDFLAG(IS_WIN)
   STARTUPINFOW si;
